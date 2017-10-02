9fb81faa3ca812b763a7a29caa585e0fd1606077
rsn95 <32263826+rsn95@users.noreply.github.com>
2017-10-02 13:46:59 +0200

    cwiczenie 2

---
+++ cwiczenie 1/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+
+int main(int argc, char* argv[])
+
+{
+        int i,j;
+  {
+
+
+    for (i = 0; i<40; i++)
+            cout<<" ";
+            cout<<endl;
+    for (j = 0; j<1; j++)
+            cout<<"*";
+            cout<<endl;
+
+
+  }
+
+
+
+
+    system("PAUSE");
+    return 0;
+
+}
